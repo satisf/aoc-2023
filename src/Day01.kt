2fff9f26d9ec68d4aaf92f91bbb42f4bca5dd2d5
satisf <ppmoeller@posteo.net>
2023-12-01 15:23:18 +0100

    01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,54 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map {line ->
+            ((line.findFirstDigit() ?: "") + (line.reversed().findFirstDigit() ?: "")).toInt()
+        }.reduce { acc, next -> acc + next }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+                .map {
+                    it.replaceWords()}
+                .map {line ->
+                    ((line.findFirstDigit() ?: "") + (line.reversed().findFirstDigit() ?: "")).toInt() }
+                .reduce { acc, next -> acc + next }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+     val testInput = readInput("01_test")
+     check(part2(testInput) == 281)
 
-    val input = readInput("Day01")
+    val input = readInput("01")
     part1(input).println()
     part2(input).println()
 }
+
+private fun String.findFirstDigit(): String? {
+    return this.find { it.isDigit() }?.toString()
+}
+
+private fun String.replaceWords(): String {
+    val stringBuilder = StringBuilder(this)
+    stringBuilder.findAnyOf(DIGIT_WORDS)?.let {
+        stringBuilder.insert(it.first, it.second.wordsToDigits())
+    }
+    stringBuilder.findLastAnyOf(DIGIT_WORDS)?.let {
+        stringBuilder.insert(it.first, it.second.wordsToDigits())
+    }
+    return stringBuilder.toString()
+}
+
+private fun String.wordsToDigits(): String {
+    return this
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9")
+}
+
+val DIGIT_WORDS = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
