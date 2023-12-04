6be5a088d09cc60d4fd967bcd89df7e99e881512
satisf <ppmoeller@posteo.net>
2023-12-04 10:50:53 +0100

    04

--- src/Day4.kt
+++ src/Day4.kt
@@ -2,7 +2,18 @@ import java.util.StringTokenizer
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        return input.map { line ->
+            val numbers = line.split(':')[1]
+            val win = numbers.split('|')[0]
+                .split(' ')
+                .filter { it.isNotBlank() }
+                .map { it.toInt() }
+            val have = numbers.split('|')[1]
+                .split(' ')
+                .filter { it.isNotBlank() }
+                .map { it.toInt() }
+            win.intersect(have.toSet()).foldIndexed(0) {idx,acc, _ -> if (idx == 0) 1 else acc * 2}
+        }.sumUp()
     }
 
     fun part2(input: List<String>): Int {
@@ -10,11 +21,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("X_test")
-    check(part1(testInput) == 0)
+    val testInput = readInput("04_test")
+    check(part1(testInput) == 13)
     //check(part2(testInput) == 0)
 
-    val input = readInput("X")
+    val input = readInput("04")
     part1(input).println()
     part2(input).println()
 }
